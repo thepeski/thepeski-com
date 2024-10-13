f66988db790072c4215e3c2a50fb5cc220750abf
thepeski <jpeski@icloud.com>
2024-10-13 18:54:18 +0200

    added dark mode hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // react imports
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // component imports
 import Navbar from "./components/Navbar";
 
+// hooks imports
+import useDarkMode from "./hooks/useDarkMode";
+
 function App() {
+    const [isDarkMode, setIsDarkMode] = useDarkMode();
+    const toggleDarkMode = () => setIsDarkMode((mode) => !mode);
 
     return (
         <Router>
             <Navbar />
+            <br/><br/>
+            <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+            <p>{isDarkMode?"true":"false"}</p>
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
                 <Route path="/:domain" element={<h1>Domain</h1>} />
---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,26 @@
+import { React, useState, useEffect } from "react";
+
+function useDarkMode() {
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        const darkModePreference = localStorage.getItem("theme"); // check if preference is saved in localStorage
+        return darkModePreference === "dark" ? true : darkModePreference === "light" ? false : true; // default to dark mode
+    });
+
+    useEffect(() => {
+        const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)"); // get current color preference
+
+        const handleChange = (e) => setIsDarkMode(e.matches); // trigger setDarkMode when color preference changes
+        darkModeMediaQuery.addEventListener("change", handleChange); // listen for changes
+        return () => darkModeMediaQuery.removeEventListener("change", handleChange); // stop listening when component unmounts
+    }, []); // run effect only once
+
+    useEffect(() => {
+        if (isDarkMode) localStorage.setItem("theme", "dark");
+        else localStorage.setItem("theme", "light");
+    }, [isDarkMode]); // run effect when isDarkMode changes
+
+
+    return [isDarkMode, setIsDarkMode];
+}
+
+export default useDarkMode;
