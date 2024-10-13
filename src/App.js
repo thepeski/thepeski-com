ed99901d1d5b6a68ceb91e4ca0bcc7a63dda468e
thepeski <jpeski@icloud.com>
2024-10-13 18:36:46 +0200

    cleaned branch

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 // react imports
-import {React, useEffect} from "react";
-import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-// local imports
-import "./index.css";
+// component imports
 import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-
-// hooks
-import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
-    let path = {domain: "finance", topic: "corporate-finance", article: "stock-valuation", paragraph: "determining-discount-rate"};
-
-    // dark mode
-    const [darkMode, setDarkMode] = useDarkMode();
-    const toggleTheme = () => setDarkMode((prev) => !prev);
-    useEffect(() => {
-        document.body.setAttribute("data-theme", darkMode ? "dark" : "light");
-    }, [darkMode]);
 
     return (
         <Router>
-            <Navbar 
-                path={path}
-                darkMode={darkMode} 
-                toggleTheme={toggleTheme}
-            />
-            <Wrapper 
-                path={path}
-                darkMode={darkMode}
-            />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/:domain" element={<h1>Domain</h1>} />
+            </Routes>
         </Router>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,12 @@
 import React from "react";
 
-import { ReactComponent as DarkModeIcon } from "../assets/icons/darkMode.svg";
-
-import NavbarPath from "./NavbarPath";
-
-function Navbar({ path, darkMode, toggleTheme}) {
-
+function Navbar({ isDarkMode, toggleDarkMode }) {
     return (
-        <nav
-            className={`
-                fixed top-0 w-full border-b shadow-lg
-                ${darkMode ? "bg-navDark border-borderDark" : "bg-navLight border-borderLight"}
-            `}
+        <nav className="
+                fixed top-0 w-full shadow-lg
+            "
         >
-            <div className="flex justify-items items-center max-w-7xl mx-auto px-2 md:px-4 py-4">
-            
-                <NavbarPath 
-                    path={path} 
-                    darkMode={darkMode}
-                />
-
-                <DarkModeIcon className={`
-                    w-6 h-6 ml-auto cursor-pointer
-                    ${darkMode ? "fill-white" : "fill-blueLight"}`}
-                    onClick={toggleTheme} />
-            </div>
+            <p>test</p>
         </nav>
     );
 }
